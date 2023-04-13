4b4a46b3afc5e8aecad7d602b36752278f9ae486
yura teslyuk <116177992+yurickv@users.noreply.github.com>
2023-04-13 12:49:48 +0300

    123

---
+++ test.js
@@ -0,0 +1,93 @@
+// function makeTransaction(pricePerDroid, orderedQuantity, customerCredits) {
+//   let message;
+//   const totalPrice = pricePerDroid * orderedQuantity
+  
+
+//   // Change code below this line
+//   if (totalPrice > customerCredits) {
+//   message = `Insufficient funds! `;
+  
+//   } else {
+//   customerCredits -= totalPrice;
+//   message = `You ordered ${orderedQuantity} droids, you have ${customerCredits} credits left`;
+  
+//   // Change code above this line
+//   }
+//   return message;
+// }
+// console.log(makeTransaction(300, 5, 2000));
+
+// const courseTopic = "JavaScript essentials";
+// // Change code below this line
+
+// const courseTopicLength = courseTopic.length;
+// const firstElement = courseTopic[0];
+// const lastElement = courseTopic[19];
+// console.log(firstElement);
+// console.log(lastElement);
+
+
+// function getExtremeElements(array) {
+//   // Change code below this line
+   
+//     return array = [array[0], array[array.length -1]];
+    
+//     const clients = [array[0], array[array.length -1]];
+//     console.log(clients);
+//     return array = [array[0], array[array.length -1]];
+// // return array[0];
+//   // Change code above this line
+// }
+// getExtremeElements([1, 2, 3, 4, 5])
+// getExtremeElements(["Earth", "Mars", "Venus"])
+
+
+
+
+// function slugify(title) {
+//   // Change code below this line
+    
+//     console.log(title.toLowerCase().split(" ").join("-"));
+  
+//     // return title.toLowerCase().split(" ").join("-");
+
+
+//   // Change code above this line
+// }
+
+// slugify("Arrays for begginers")
+
+// рахує суму чисел
+// function calculateTotalPrice(order) {
+//   let total = 0;
+//   // Change code below this line
+// for (let i = 0; i < order.length; i += 1) {
+//   console.log(order[i]);
+//   total += Number(order[i]);
+//     }
+//     console.log(total);
+//     return total;
+//   // Change code above this line
+  
+// }
+// calculateTotalPrice([164, 48, 291]);
+
+
+// знайти найдовше слово
+// function findLongestWord(string) {
+//   // Change code below this line
+//     let fraze = string.split(' ');
+//     let longsWord = fraze[0];
+//     console.log(fraze);
+    
+//     for (let i = 0; i < fraze.length; i += 1) {
+//         if (longsWord.split('').length < fraze[i].split('').length) {
+//             longsWord = fraze[i];
+//         }
+//     }
+//     console.log(longsWord);
+//   // Change code above this line
+// }
+// findLongestWord("The quick brown fox jumped over the lazy dog");
+// findLongestWord("Застосування цього методу корисне в ситуаціях, коли необхідно перевірити, чи є елемент в масиві та не важлива його позиція.")
+
